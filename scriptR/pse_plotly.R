71746d25a2a9ab63efd2b92fb10aba0a381db51d
Etienne D. <etienne.delay@cirad.fr>
2024-12-06 09:14:32 +0000

    last run before close

--- scriptR/pse_plotly.R
+++ scriptR/pse_plotly.R
@@ -13,6 +13,8 @@ names(df)
 df$mTotalTreesOld <- sapply(df$totalTreesOldes, pp_mean)
 df$mTotalcattles <- sapply(df$totalCattles, pp_mean)
 df$mTotalsheeps <- sapply(df$totalSheeps, pp_mean)
+df$mCattlesLiveWeight <- sapply(df$meanCattlesLiveWeight, pp_mean)
+
 
 ggplot(df, aes(x = objective.MSTCattleNEC, y = objective.MSTTrees, colour = goodShepherdPercentage, size = evolution.samples))+
   geom_point()+
@@ -25,35 +27,13 @@ library(plotly)
 sel <- df$evolution.samples >= 1
 
 # Création du graphique avec des info-bulles personnalisées
-plot_ly(data = df[sel,],
+a <- plot_ly(data = df[sel,],
         x = ~objective.MSTCattleNEC, 
         y = ~objective.MSTTrees, 
-        type = 'scatter', 
-        mode = 'markers', 
-        color = ~interfaceNumberOfCampI,  # Coloration par nbBoats
-        #size = ~evolution.samples,  # Taille par evolution.samples
-        marker = list(sizemode = 'diameter'),
-        text = ~paste("goodShepherdPercentage: ", goodShepherdPercentage, 
-                      "<br>proportionBigHerders: ", proportionBigHerders, 
-                      "<br>proportionMediumHerders: ", proportionMediumHerders,
-                      "<br>interfaceNumberOfCampI: ", interfaceNumberOfCampI),
-        hoverinfo = 'text') %>%
-  layout(title = 'Scatter Plot with Custom Popups',
-         xaxis = list(title = 'Objective MSTCattle'),
-         yaxis = list(title = 'Objective MSTTrees'),
-         plot_bgcolor = 'rgba(0,0,0,0)',  # Correspond à theme_bw()
-         paper_bgcolor = 'rgba(0,0,0,0)')
-
-
-
-# Création du graphique avec des info-bulles personnalisées
-plot_ly(data = df[sel,],
-        x = ~objective.MSTCattleNEC, 
-        y = ~objective.MSTTrees, 
-        z = ~goodShepherdPercentage,   # Axe Z (3ème dimension)
+        z = ~mTotalcattles,   # Axe Z (3ème dimension)
         type = 'scatter3d', 
         mode = 'markers', 
-        color = ~mTotalcattles,  # Coloration par nbBoats
+        color = ~mCattlesLiveWeight,  # Coloration par nbBoats
         #size = ~evolution.samples,  # Taille par evolution.samples
         marker = list(sizemode = 'diameter'),
         text = ~paste("goodShepherdPercentage: ", goodShepherdPercentage, 
@@ -62,18 +42,19 @@ plot_ly(data = df[sel,],
                       "<br>interfaceNumberOfCampI: ", interfaceNumberOfCampI,
                       "<br>TotalOldTree: ", mTotalTreesOld,
                       "<br>TotalCattles: ", mTotalcattles,
-                      "<br>TotalSheeps: ", mTotalsheeps
+                      "<br>TotalSheeps: ", mTotalsheeps,
+                      "<br>mCattlesLiveWeight: ", mCattlesLiveWeight
                       ),
         hoverinfo = 'text') %>%
   layout(title = 'Scatter Plot with Custom Popups',
          xaxis = list(title = 'Objective MSTCattle'),
          yaxis = list(title = 'Objective MSTTrees'),
-         zaxis = list(title = 'goodShepherdPercentage'),
+         zaxis = list(title = 'mTotalcattles'),
          plot_bgcolor = 'rgba(0,0,0,0)',  # Correspond à theme_bw()
          paper_bgcolor = 'rgba(0,0,0,0)')
 
 
-
+a
 
 
 
