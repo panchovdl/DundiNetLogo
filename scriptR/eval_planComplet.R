b4a1ab6d6d9fcbe78e72d1ba400dd899a781782b
Etienne D. <etienne.delay@cirad.fr>
2025-03-16 10:44:36 +0000

    modified path and geom_boxplot

--- scriptR/eval_planComplet.R
+++ scriptR/eval_planComplet.R
@@ -5,20 +5,7 @@ library(tidyverse)
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-myPath <- '../results/'
-files.l <- list.files(path = myPath, pattern = "M0_directSampling_muse",)
-
-###############################################################################
-# Lecture du fichier CSV
-###############################################################################
-# Les fichiers "M0_directSampling_muse_*" sont lu dans une boucle qui itère les noms 
-# avec la première ligne considérée comme en-têtes de colonnes (header = TRUE).
-data <- data.frame()
-for(i in 1:length(files.l)){
-  tmp <- read.csv(paste0(myPath,files.l[i]), header = TRUE)
-  data <- rbind(data, tmp)
-}
-
+data <- readRDS('../results/M0_directSampling_muse_merged.RDS')
 ###############################################################################
 # Sélection d'un sous-ensemble de colonnes
 ###############################################################################
@@ -52,7 +39,7 @@ sub.data <- subset(
 # 1) lapply(sub.data, table) : Applique la fonction table à chacune des 
 #    colonnes séparément, retournant une liste de tables de fréquences 
 #    (une par colonne).
-lapply(sub.data, table)
+#lapply(sub.data, table)
 
 
 
@@ -61,6 +48,6 @@ sel <- data$proportionBigHerders == 50 & data$proportionMediumHerders == 50 &
 
 
 ggplot( data = data[sel,])+
-  geom_point(aes(x = as.factor(goodShepherdPercentage), y = sheepsTempCamp))+
-  # ylim(c(0,1))+
+  geom_boxplot(aes(x = as.factor(goodShepherdPercentage), y = sheepsTempCamp),outliers = FALSE)+
+  labs(x="% good Shepherd", y = "nb temp. camps for sheeps")+
   theme_bw()
